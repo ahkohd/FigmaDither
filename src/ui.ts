b778db17860552c57a7855d7a7fcb98d0b7e04da
Victor Aremu <victor.olorunbunmi@gmail.com>
2019-09-09 09:10:48 +0100

    Wired options input ✔

--- src/ui.ts
+++ src/ui.ts
@@ -3,10 +3,21 @@ import './figma-plugin-ds.min.css';
 import './ui.css';
 
 document.getElementById('dither').onclick = () => {
-  // const textbox = document.getElementById('resolution') as HTMLInputElement;
-  // const count = parseInt(textbox.value, 10);
-  parent.postMessage({ pluginMessage: { type: 'dither-image', count: 0 } }, '*');
+  const options = {
+    greyscale_method: getValue('greyscale_method'),
+    dither_method: getValue('dither_method'),
+    threshold: getValue('threshold'),
+    chk_replace_colours: getValue('chk_replace_colours'),
+    rep_black: [getValue('rep_black_r'), getValue('rep_black_b'), getValue('rep_black_g'), getValue('rep_black_a')],
+    rep_white: [getValue('rep_white_r'), getValue('rep_white_b'), getValue('rep_white_g'), getValue('rep_white_a')]
+  }
+  console.log(options);
+  parent.postMessage({ pluginMessage: { type: 'dither-image', options: options } }, '*');
+}
 
+function getValue(selector)
+{
+  return (document.getElementById(selector) as HTMLInputElement).value;
 }
 
 document.getElementById('cancel').onclick = () => {
